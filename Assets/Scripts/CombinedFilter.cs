cf36af1131420755af4f70f63087fb953275da60
unknown <c00214190@itcarlow.ie>
2019-09-27 11:19:24 +0100

    Have submodules added, and combined filter created and tested

---
+++ Assets/Scripts/CombinedFilter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+public class CombinedFilter
+{
+
+    public static List<int> combinedFilter(List<int> inputNumbers)
+    {
+        return RunningTotalFilter.runningTotal(Modulo3.modulo3(XOR21Filter.XOR21s(inputNumbers.ToArray())).ToList<int>());
+    }
+
+}
---
+++ Assets/Tests/CombinedFilterTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CombinedFilterTests
+    {
+        [Test]
+        public void CombineFilterTest()
+        {
+            List<int> input = new List<int> { 0, 1, 2, 3, 4, 5, 696969420 };
+            List<int> output = CombinedFilter.combinedFilter(input);
+            List<int> expected = new List<int> { 0, 2, 4, 5, 7, 8, 9 };
+
+            List<int> wrongValues = new List<int> { 1, 2, 3, 4, 5 };
+            CollectionAssert.AreNotEqual(output, wrongValues);
+            CollectionAssert.AreEqual(output, expected);
+        }
+    }
+}
